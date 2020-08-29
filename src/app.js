f61ff0ea243d58ee9d0b9fdae577d6ae312486dd
Archie Hilton (thytom) <archie.hilton1@gmail.com>
2020-08-29 23:31:10 +0100

    Minor refactors

--- src/app.js
+++ src/app.js
@@ -14,12 +14,13 @@ for (const file of commandFiles) {
 	client.commands.set(command.name, command);
 }
 
-client.on('ready', function (evt) {
+client.on('ready', () => {
 	const guilds = client.guilds.cache.map(x => x.name);
-	var guildList = "";
-	for(const g in guilds) {
-		guildList += `  ${g} - ${guilds[g]}\n`;
-	}
+	let guildList = "";
+
+	for(const guild in guilds)
+		guildList += `  ${guild} - ${guilds[guild]}\n`;
+	
 	console.log('Logged into ' + guilds.length + ' guilds: \n' + guildList);
 });
 
@@ -29,13 +30,13 @@ client.on('message', message => {
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (!client.commands.has(command)) return;
-
-	try {
-		client.commands.get(command).execute(message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
+	if (client.commands.has(command)) {
+		try {
+			client.commands.get(command).execute(message, args);
+		} catch (error) {
+			console.error(error);
+			message.reply('there was an error trying to execute that command!');
+		}
 	}
 });
 
